5d48fc154b790a0aa0dc96afd88a79599f914837
bonstriver <brandon.martinez0502@gmail.com>
2022-07-27 00:10:21 -0700

    Co-authored-by: Isaac Hansen <isaachansen@users.noreply.github.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
                         <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                            Home
+                            Checkers (coming soon)
                         </Link>
                     </li>
                 </ul>
